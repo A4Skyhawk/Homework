882a5db76782b832b99fef4377637fdc3e0dca0a
Михаил Кузнеделев <90871502+A4Skyhawk@users.noreply.github.com>
2021-11-10 19:12:45 +0300

    Add files via upload

---
+++ 07-11-2021_4.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if (b >= a) & (a <= c):
+    print(a)
+elif (a >= b) & (b <= c):
+    print(b)
+else:
+    print(c)
---
+++ 07-11-2021_8.py
@@ -0,0 +1,11 @@
+n = int(input())
+m = int(input())
+x = int(input())
+y = int(input())
+go1 = (n-x)
+go2 = (m-y)
+
+if go1 < go2:
+    print(go1)
+else:
+    print(go2)
