d0a26a5a243cc6c9cc5c914a4e1c55f22f806c9a
Михаил Кузнеделев <90871502+A4Skyhawk@users.noreply.github.com>
2022-01-05 20:27:09 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,13 +1,13 @@
 action = input()
 n1 = int(input())
 n2 = int(input())
-if (action=='*')
+if action=='*':
 	print (n1 * n2);
-if (action=='+')
+elif action=='+':
 	print (n1 + n2);
-if (action=='-')
+elif action=='-':
 	print (n1 - n2);
-if (action=='/' and n2!=0)
+elif (action=='/') and (n2!=0):
 	print (n1 / n2);
-if (n2 == 0):
+elif (action=='/') and (n2==0):
     print ('AMOGUS')
